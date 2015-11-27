f1e1d6016372f6f29885aa6cdc0fbf708fce14cd
Marcus Johnson <mejohnson93@gmail.com>
2015-11-27 13:51:57 -0700

    add require to tsd

--- src/typings/tsd.d.ts
+++ src/typings/tsd.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="../../bower_components/phaser/typescript/p2.d.ts" />
 /// <reference path="../../bower_components/phaser/typescript/phaser.comments.d.ts" />
 /// <reference path="../../bower_components/phaser/typescript/pixi.comments.d.ts" />
+/// <reference path="../../typings/requirejs/require.d.ts" />
