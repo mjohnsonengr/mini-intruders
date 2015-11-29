e765d0d558a344fa5af75db977ebf4ed78a6f816
Marcus Johnson <mejohnson93@gmail.com>
2015-11-29 06:56:21 -0700

    fix indentation
    (forgot to do this when switching modules)

--- src/Level.ts
+++ src/Level.ts
@@ -1,18 +1,18 @@
 import {Player} from "./Player";
 
-    export class Level extends Phaser.State {
+export class Level extends Phaser.State {
 
-        private background: Phaser.Sprite;
-        private player: Player;
+    private background: Phaser.Sprite;
+    private player: Player;
 
-        public create() {
+    public create() {
 
-            //this.physics.startSystem(Phaser.Physics.ARCADE);
+        //this.physics.startSystem(Phaser.Physics.ARCADE);
 
-            this.background = this.add.sprite(0, 0, 'background01');
+        this.background = this.add.sprite(0, 0, 'background01');
 
-            this.player = new Player(this.game, this.game.world.centerX, this.game.world.height*0.8);
-
-        }
+        this.player = new Player(this.game, this.game.world.centerX, this.game.world.height*0.8);
 
     }
+
+}
--- src/Player.ts
+++ src/Player.ts
@@ -1,28 +1,28 @@
-    export class Player extends Phaser.Sprite {
+export class Player extends Phaser.Sprite {
 
-        constructor(game: Phaser.Game, x: number, y: number) {
+    constructor(game: Phaser.Game, x: number, y: number) {
 
-            super(game, x, y, 'sprites', 'fighter1.png');
+        super(game, x, y, 'sprites', 'fighter1.png');
 
-            this.angle = -90;
-            this.anchor.setTo(0.5, 0);
+        this.angle = -90;
+        this.anchor.setTo(0.5, 0);
 
-            game.physics.enable(this);
+        game.physics.enable(this);
 
-            game.add.existing(this);
+        game.add.existing(this);
+    }
+
+    public update() {
+
+        this.body.velocity.x = 0;
+
+        if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+            this.body.velocity.x = -170;
         }
-
-        public update() {
-
-            this.body.velocity.x = 0;
-
-            if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-                this.body.velocity.x = -170;
-            }
-            else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-                this.body.velocity.x = 170;
-            }
-
+        else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+            this.body.velocity.x = 170;
         }
 
     }
+
+}
