da615d5f2879fe7d3026a334ab52145501799c11
Marcus Johnson <mejohnson93@gmail.com>
2015-11-29 07:50:29 -0700

    Added some shortcuts for input handling in Player

--- src/Player/Player.ts
+++ src/Player/Player.ts
@@ -1,3 +1,5 @@
+import keys = Phaser.Keyboard; // shortcut
+
 export class Player extends Phaser.Sprite {
 
     constructor(game: Phaser.Game, x: number, y: number, key?: string|Phaser.RenderTexture|Phaser.BitmapData|PIXI.Texture, frame?: string|number) {
@@ -14,15 +16,16 @@ export class Player extends Phaser.Sprite {
 
     public update() {
 
+        // side-to-side
         this.body.velocity.x = 0;
-
-        if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+        if (this.keyDown(keys.LEFT)) {
             this.body.velocity.x = -170;
         }
-        else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+        else if (this.keyDown(keys.RIGHT)) {
             this.body.velocity.x = 170;
         }
 
     }
+    private keyDown = (key: number) => this.game.input.keyboard.isDown(key);
 
 }
