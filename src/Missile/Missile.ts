29ecd5c8ff5638fa3b360c636b52eff5bcfef3f7
Marcus Johnson <mejohnson93@gmail.com>
2015-12-04 23:14:56 -0700

    The basic missile object
    Can be fired with an initial velocity
    killed when off-screen for re-use
    No collision

---
+++ src/Missile/Missile.ts
@@ -0,0 +1,27 @@
+export abstract class Missile extends Phaser.Sprite {
+
+    constructor(game: Phaser.Game, x: number, y: number,
+        key?: string|Phaser.RenderTexture|Phaser.BitmapData|PIXI.Texture, frame?: string|number) {
+        super(game, x, y, key, frame);
+
+        game.physics.enable(this);
+
+        // enable object recycling
+        this.checkWorldBounds = true;
+        this.outOfBoundsKill = true;
+
+        game.add.existing(this);
+    }
+
+    public fire(pos: Phaser.Point, velocity: Phaser.Point): void {
+        this.exists = true;
+        this.body.velocity = velocity;
+        this.x = pos.x;
+        this.y = pos.y;
+    }
+
+    public update(): void {
+        // TODO collision
+        // hit player or enemy? Call their "shot" methods and delete bullet
+    }
+}
